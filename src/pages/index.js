f695b235bba5e9ea00896034bac734186cde09b6
spencercv7 <spencercv7@gmail.com>
2021-05-11 17:42:40 -0400

    removed non-prod material

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,34 +2,34 @@ import { createClient } from 'contentful';
 import styled from 'styled-components';
 
 import LandingPage from '../components/home/LandingPage';
-import Schedule from '../components/schedule/Schedule';
+// import Schedule from '../components/schedule/Schedule';
 import About from '../components/about/About';
 import Contact from '../components/contact/Contact';
 import Credit from '../components/credit/Credit';
 
-export const getStaticProps = async () => {
+// export const getStaticProps = async () => {
 
-    const client = createClient({
-        space: process.env.CONTENTFUL_SPACE_ID,
-        accessToken: process.env.CONTENTFUL_ACCESS_KEY
-    });
+//     const client = createClient({
+//         space: process.env.CONTENTFUL_SPACE_ID,
+//         accessToken: process.env.CONTENTFUL_ACCESS_KEY
+//     });
 
-    const res_schedule = await client.getEntries({ content_type: 'schedule' });
+//     const res_schedule = await client.getEntries({ content_type: 'schedule' });
 
-    return {
-        props: {
-            schedule: res_schedule.items
-        }
-    }
-}
+//     return {
+//         props: {
+//             schedule: res_schedule.items
+//         }
+//     }
+// }
 
-const Home = ({ schedule }) => {
+const Home = () => {
 
     return (
         <div>
             <LandingPage />
             <About />
-            <Schedule schedule= { schedule }/>
+            {/* <Schedule schedule= { schedule }/> */}
             <Contact />
             <Credit />
         </div>
