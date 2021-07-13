62672c8f195e68028c887cdc77a78bd11d426bf3
spencercv7 <spencercv7@gmail.com>
2021-07-13 16:50:54 -0400

    Added new site description.

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -14,7 +14,7 @@ export default class MyDocument extends Document {
                     <link rel="preload" href="/fonts/Staatliches-Regular.woff2" as="font" crossOrigin="anonymous" />
                     <meta
                         name="description"
-                        content="Manny Houston's website. Your place for all things Manny Houston, where you can find extra content, and a behind the scenes look at the man behind the art."
+                        content="Manny Houston, a creative from South Carolina, Actor, Rapper, Pianist, working to inspire young black kids to be creative."
                     />
                 </Head>
                 <body>
